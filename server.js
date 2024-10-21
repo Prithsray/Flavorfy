93e717807466fb62149afc8f6c0e17bd3f56f56b
Prithwish Raymahapatra <85878380+Prithsray@users.noreply.github.com>
2024-10-21 20:00:30 +0530

    update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors({ origin: 'https://flavorfy.vercel.app/', credentials: true })); // Enable CORS for specific origin
+app.use(cors({ origin: 'https://flavorfy.vercel.app', credentials: true })); // Enable CORS for specific origin
 app.use(express.json());
 app.use(session({
   secret: 'your_session_secret', // Change this to a more secure secret
