81fd33f602bce4c72e58e91cae79f5ff52a26e8b
Prithwish Raymahapatra <85878380+Prithsray@users.noreply.github.com>
2024-10-22 02:45:05 +0530

    update server.js

--- server.js
+++ server.js
@@ -7,13 +7,15 @@ const session = require('express-session'); // Import express-session
 const recipeRoutes = require('./routes/recipes');
 const registerRoutes = require('./routes/register');
 const loginRoutes = require('./routes/login');
-const profileRoutes= require('./routes/profile');
-const searchRoutes=require('./routes/search')
+const profileRoutes = require('./routes/profile');
+const searchRoutes = require('./routes/search');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const FRONTEND_API_BASE_URL = process.env.FRONTEND_URL;
-console.log(FRONTEND_API_BASE_URL)
+
+console.log(FRONTEND_API_BASE_URL);
+
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({ origin: `${FRONTEND_API_BASE_URL}`, credentials: true })); // Enable CORS for specific origin
@@ -32,7 +34,8 @@ app.use('/api/register', registerRoutes);
 app.use('/api/login', loginRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/search', searchRoutes);
+
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
